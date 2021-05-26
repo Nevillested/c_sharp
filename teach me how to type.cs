69069715f2ce6b85d38be327c67babfaf36b815f
Oleg <olegsidnev@gmail.com>
2021-05-26 17:18:39 +0300

    Update teach me how to type.cs

--- teach me how to type.cs
+++ teach me how to type.cs
@@ -8,15 +8,48 @@ using System.Threading.Tasks;
 
 namespace C_sharp
 {
+
     class Program
     {
+        public static int x = 1; //счетчик попыток
+        public static double max = 0.0; //лучший результат
+        public static double min = 0.0; //худший результат
+        public static double avg = 0.0; //средний результат
+        class result
+        {
+
+
+            public void getresult(ref int random, double dime, TimeSpan span)
+            {
+                double V = (options[random].Length / dime);
+                if ((min == 0.0) || (max == 0.0) || (avg == 0.0))
+                {
+                    min = max = avg = V;
+                }
+                else
+                {
+                    if (min > V)
+                    {
+                        min = V;
+                    }
+                    if (max < V)
+                    {
+                        max = V;
+                    }
+                    avg = (avg + V) / x;
+                }
+                Console.WriteLine("\r\nПопытка:" + (x) + "\r\nОтлично, твоё время: " + (span.Seconds) + " секунд." + "\r\nКоличество символов: {0}", options[random].Length + "\r\nТвоя корость: " + Math.Round(V, 2) + " зн/мин\r\nСамая маленькая скорость: " + Math.Round(min, 2) + " зн/мин" + "\r\nСамая большая скорость: " + Math.Round(max, 2) + " зн/мин" + "\r\nСредняя скорость: " + Math.Round(avg, 2) + " зн/мин");
+                x++; //инкрементное прибавление к счетчику попыток каждый круг
+            }
+        }
         public static Dictionary<string, string[]> dict = new Dictionary<string, string[]>(); //создаем словарь
         public static string keyy = "0";
         public static string[] options = new[] { "Шла Маша по Шоссе и сосала сушку.", "Бык тупогуб, тупогубенький бычок, у быка губа тупа.", "Во дворе трава, на траве дрова", "От топота копыт пыль по полю летит.", "Корабли лавировали, лавировали, да не вылавировали.", "Выдра в ведро от выдры нырнула. Выдра в ведре с водой утонула." }; //создание русского массива
         public static string[] options2 = new[] { "Two wrongs don't make a right.", "The pen is mightier than the sword.", "When in Rome, do as the Romans.", "The squeaky wheel gets the grease.", "When the going gets tough, the tough get going.", "No man is an island.", "Fortune favors the bold." }; //создание английского массива
         public static int k = 0;
-        static int Minimum(int a, int b, int c) => (a = a < b ? a : b) < c ? a : c; //получение минимального из трех расстояния Левенштейна
-        static int LevenshteinDistance(string firstWord, string secondWord) //Итеративная реализация Левенштейна
+
+        public static int Minimum(int a, int b, int c) => (a = a < b ? a : b) < c ? a : c; //получение минимального из трех расстояния Левенштейна
+        public static int LevenshteinDistance(string firstWord, string secondWord) //Итеративная реализация Левенштейна
         {
             var n = firstWord.Length + 1;
             var m = secondWord.Length + 1;
@@ -69,12 +102,6 @@ namespace C_sharp
         {
             Console.WriteLine("Давай замерим скорость твоей печати. \r\nНапиши текст, который будет показан ниже. \r\nВыбери словарь (eng/rus):");
             vibor();
-            var x = 1; //счетчик попыток
-            var max = 0.0; //лучший результат
-            var min = 0.0; //худший результат
-            var avg = 0.0; //средний результат
-
-
             do
             {
                 Random rand = new Random(); //метод рандома
@@ -84,28 +111,16 @@ namespace C_sharp
                 string stroka = Console.ReadLine(); // предложение ввести текст и присваивание тексту переменную stroka
                 TimeSpan span = DateTime.Now - startedAt; //вычисляет время, за которое напечатан текст
                 double dime = (DateTime.Now - startedAt).TotalMinutes; //переводит в дабл минуты
+
+
+                result popitka = new result();
+
                 if (String.Compare(dict[keyy][random], stroka) == 0) //если ошибок нет, то...
                 {
-                    double V = (options[random].Length / dime);
-                    if ((min == 0.0) || (max == 0.0) || (avg == 0.0))
-                    {
-                        min = max = avg = V;
-                    }
-                    else
-                    {
-                        if (min > V)
-                        {
-                            min = V;
-                        }
-                        if (max < V)
-                        {
-                            max = V;
-                        }
-                        avg = (avg + V) / x;
-                    }
-                    Console.WriteLine("\r\nПопытка:" + (x) + "\r\nОтлично, твоё время: " + (span.Seconds) + " секунд." + "\r\nКоличество символов: {0}", options[random].Length + "\r\nТвоя корость: " + Math.Round(V, 2) + " зн/мин\r\nСамая маленькая скорость: " + Math.Round(min, 2) + " зн/мин" + "\r\nСамая большая скорость: " + Math.Round(max, 2) + " зн/мин" + "\r\nСредняя скорость: " + Math.Round(avg, 2) + " зн/мин");
-                    x++; //инкрементное прибавление к счетчику попыток каждый круг
+                    popitka.getresult(ref random, dime, span);
                 }
+
+
                 else //если ошибки есть, то...
                 {
                     var s1 = dict[keyy][random];
